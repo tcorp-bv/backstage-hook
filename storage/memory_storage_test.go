4ee3c1cca057784faf447c4b9ae1ad7f1cbe96e3
Toon Sevrin <twan123@live.be>
2020-03-28 01:33:00 +0100

    Update memory_storage_test.go

--- storage/memory_storage_test.go
+++ storage/memory_storage_test.go
@@ -62,7 +62,7 @@ func TestSessionDeletion(t *testing.T) {
 	key := "somehash"
 
 	val, contains := s.Get(key)
-	if contains || (val != StoredSession{}) {
+        if contains || (val != StoredSession{}) {
 		t.Error("Session should not be fetchable when it was not stored.")
 	}
 
